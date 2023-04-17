484d0cf10c368e32390bc5f41df936d8d85df980
Fede <fnotalvares@gmail.com>
2023-04-17 20:32:54 +0200

    Import classes from got.js

---
+++ app/js/characters/Character/Character.js
@@ -0,0 +1,17 @@
+export class Character {
+  isAlive = true;
+  series = "Game of Thrones";
+  characterData;
+
+  constructor(characterData) {
+    this.characterData = characterData;
+  }
+
+  communicate() {
+    return `${this.characterData.name} says: `;
+  }
+
+  die() {
+    this.isAlive = false;
+  }
+}
---
+++ app/js/characters/King/King.js
@@ -0,0 +1,14 @@
+import { Character } from "../Character/Character.js";
+
+export class King extends Character {
+  yearsOfReign;
+
+  constructor(characterData, yearsOfReign) {
+    super(characterData);
+    this.yearsOfReign = yearsOfReign;
+  }
+
+  communicate() {
+    return `${super.communicate()}Everybody will die`;
+  }
+}
