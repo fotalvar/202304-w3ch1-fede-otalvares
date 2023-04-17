d09e084668beadd87fcd07ff4bf24404ae471be9
Fede <fnotalvares@gmail.com>
2023-04-17 23:18:38 +0200

    Add ButtonComponent and index.js instance

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,6 @@
-import characters from "./characters/characters.js";
+import { characters } from "./characters/characters.js";
+import ButtonComponent from "./components/ButtonComponent/ButtonComponent.js";
+
+const app = document.querySelector(".app");
+
+const button = new ButtonComponent(app, "speak", "character__action");
